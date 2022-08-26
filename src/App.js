e351cadc326e65fcd965e369a4a4c4d68aa021cb
ldcnft <101517909+ldcnft@users.noreply.github.com>
2022-08-26 21:21:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -236,9 +236,7 @@ function App() {
                 color: "var(--primary-text)",
               }}
             >
-              <StyledLink target={"_blank"} href={CONFIG.SCAN_LINK}>
-                {truncate(CONFIG.CONTRACT_ADDRESS, 15)}
-              </StyledLink>
+             
             </s.TextDescription>
             <span
               style={{
